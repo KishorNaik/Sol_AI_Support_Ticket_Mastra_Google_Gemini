6236184b9cd52e348a6b1aaac15a1420158e92c3
Kishor Naik <kishor.naik011.net@gmail.com>
2025-06-24 20:27:35 +0530

    Add tryCatchFinallyResultAsync utility and improve parameter validation

--- utils/src/core/shared/utils/miscellaneous/tryCatchResult/index.ts
+++ utils/src/core/shared/utils/miscellaneous/tryCatchResult/index.ts
@@ -2,10 +2,12 @@ import { StatusCodes } from "http-status-codes";
 import { ResultError, ResultExceptionFactory } from "../../exceptions/results";
 import { Result } from "neverthrow";
 
-export const tryCatchResultAsync=async<T>(action:()=>Promise<Result<T, ResultError>>):Promise<Result<T, ResultError>>=>{
+export const tryCatchResultAsync=async<T>(onTry:()=>Promise<Result<T, ResultError>>):Promise<Result<T, ResultError>>=>{
   try
   {
-    const result=await action();
+    if(!onTry)
+      return ResultExceptionFactory.error(StatusCodes.BAD_REQUEST,'Action is required');
+    const result=await onTry();
     return result;
   }
   catch(ex){
@@ -13,3 +15,25 @@ export const tryCatchResultAsync=async<T>(action:()=>Promise<Result<T, ResultErr
     return ResultExceptionFactory.error(StatusCodes.INTERNAL_SERVER_ERROR,error.message);
   }
 }
+
+export const tryCatchFinallyResultAsync = async <T>(
+  onTry: () => Promise<Result<T, ResultError>>,
+  onFinally?: () => void | Promise<void>
+): Promise<Result<T, ResultError>> => {
+  try {
+    if(!onTry)
+      return ResultExceptionFactory.error(StatusCodes.BAD_REQUEST,'Action is required');
+    const result = await onTry();
+    return result;
+  } catch (ex) {
+    const error = ex as Error;
+    return ResultExceptionFactory.error(
+      StatusCodes.INTERNAL_SERVER_ERROR,
+      error.message
+    );
+  } finally {
+    if (onFinally) {
+      await onFinally();
+    }
+  }
+};
